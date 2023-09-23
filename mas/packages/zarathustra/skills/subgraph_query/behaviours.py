e6d7dadadcca1a499ec06f6fc03b02b0247b7db6
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2023-09-23 21:35:09 +0200

    feat: subgraph_query

---
+++ mas/packages/zarathustra/skills/subgraph_query/behaviours.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2023 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This package contains round behaviours of SubgraphQueryAbciApp."""
+
+from abc import ABC
+from typing import Generator, Set, Type, cast
+
+from packages.valory.skills.abstract_round_abci.base import AbstractRound
+from packages.valory.skills.abstract_round_abci.behaviours import (
+    AbstractRoundBehaviour,
+    BaseBehaviour,
+)
+
+from packages.zarathustra.skills.subgraph_query.models import Params
+from packages.zarathustra.skills.subgraph_query.rounds import (
+    SynchronizedData,
+    SubgraphQueryAbciApp,
+    CollectedSubgraphResponseRound,
+    PrepareSubgraphQueryRound,
+)
+from packages.zarathustra.skills.subgraph_query.rounds import (
+    CollectedSubgraphResponsePayload,
+    PrepareSubgraphQueryPayload,
+)
+
+
+class SubgraphQueryBaseBehaviour(BaseBehaviour, ABC):
+    """Base behaviour for the subgraph_query skill."""
+
+    @property
+    def synchronized_data(self) -> SynchronizedData:
+        """Return the synchronized data."""
+        return cast(SynchronizedData, super().synchronized_data)
+
+    @property
+    def params(self) -> Params:
+        """Return the params."""
+        return cast(Params, super().params)
+
+
+class CollectedSubgraphResponseBehaviour(SubgraphQueryBaseBehaviour):
+    """CollectedSubgraphResponseBehaviour"""
+
+    matching_round: Type[AbstractRound] = CollectedSubgraphResponseRound
+
+    # TODO: implement logic required to set payload content for synchronization
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            sender = self.context.agent_address
+            payload = CollectedSubgraphResponsePayload(sender=sender, content=...)
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+
+class PrepareSubgraphQueryBehaviour(SubgraphQueryBaseBehaviour):
+    """PrepareSubgraphQueryBehaviour"""
+
+    matching_round: Type[AbstractRound] = PrepareSubgraphQueryRound
+
+    # TODO: implement logic required to set payload content for synchronization
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            sender = self.context.agent_address
+            payload = PrepareSubgraphQueryPayload(sender=sender, content=...)
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+
+class SubgraphQueryRoundBehaviour(AbstractRoundBehaviour):
+    """SubgraphQueryRoundBehaviour"""
+
+    initial_behaviour_cls = PrepareSubgraphQueryBehaviour
+    abci_app_cls = SubgraphQueryAbciApp  # type: ignore
+    behaviours: Set[Type[BaseBehaviour]] = [
+        CollectedSubgraphResponseBehaviour,
+        PrepareSubgraphQueryBehaviour
+    ]
